ba3233144bcc689629439fdbaad3ac258227a194
thomas Berthé <contact@berthe-thomas.fr>
2017-01-30 13:57:32 +0100

    create employe class

--- myrh-dao/src/main/java/fr/imie/myrh/dao/model/Employe.java
+++ myrh-dao/src/main/java/fr/imie/myrh/dao/model/Employe.java
@@ -1,5 +1,109 @@
 package fr.imie.myrh.dao.model;
 
-public class Employe {
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employe")
+public class Employe implements Serializable {
+
+	private static final long serialVersionUID = 3025059727134698334L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "surname")
+	private String surname;
+
+	@Column(name = "birthdate")
+	private Date birthdate;
+
+	@Column(name = "ssid")
+	private String ssid;
+
+	@Column(name = "salary")
+	private Double salary;
+
+	@Column(name = "arrival_date")
+	private Date arrival_date;
+
+	@Column(name = "departure_date")
+	private Date departure_date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getSsid() {
+		return ssid;
+	}
+
+	public void setSsid(String ssid) {
+		this.ssid = ssid;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Date getArrival_date() {
+		return arrival_date;
+	}
+
+	public void setArrival_date(Date arrival_date) {
+		this.arrival_date = arrival_date;
+	}
+
+	public Date getDeparture_date() {
+		return departure_date;
+	}
+
+	public void setDeparture_date(Date departure_date) {
+		this.departure_date = departure_date;
+	}
 
 }
