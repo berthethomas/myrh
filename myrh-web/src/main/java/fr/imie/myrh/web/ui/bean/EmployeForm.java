275256ec0ad8a986c60997074ae8e7109761a197
thomas Berthé <contact@berthe-thomas.fr>
2017-02-03 09:38:05 +0100

    mploye

---
+++ myrh-web/src/main/java/fr/imie/myrh/web/ui/bean/EmployeForm.java
@@ -0,0 +1,102 @@
+package fr.imie.myrh.web.ui.bean;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import fr.imie.myrh.dao.ifc.IDepartementDAO;
+import fr.imie.myrh.dao.impl.DepartementDAOImpl;
+import fr.imie.myrh.dao.model.Departement;
+
+@ManagedBean(name = "employe")
+public class EmployeForm {
+	
+	private final IDepartementDAO _departementDaoService = new DepartementDAOImpl();
+	
+	private String name;
+	private String surname;
+	private Date birthdate;
+	private Date arrival_date;
+	private Date departure_date;
+	private String nss;
+	private Double salary;
+	private Departement departement;
+	private List<Departement> departements;
+	
+	public EmployeForm() {
+		departements = _departementDaoService.findAll();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Date getArrival_date() {
+		return arrival_date;
+	}
+
+	public void setArrival_date(Date arrival_date) {
+		this.arrival_date = arrival_date;
+	}
+
+	public Date getDeparture_date() {
+		return departure_date;
+	}
+
+	public void setDeparture_date(Date departure_date) {
+		this.departure_date = departure_date;
+	}
+
+	public String getNss() {
+		return nss;
+	}
+
+	public void setNss(String nss) {
+		this.nss = nss;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Departement getDepartement() {
+		return departement;
+	}
+
+	public void setDepartement(Departement departement) {
+		this.departement = departement;
+	}
+
+	public List<Departement> getDepartements() {
+		return departements;
+	}
+
+	public void setDepartements(List<Departement> departements) {
+		this.departements = departements;
+	}
+}
--- myrh-web/src/main/java/fr/imie/myrh/web/ui/bean/EmployeViewController.java
+++ myrh-web/src/main/java/fr/imie/myrh/web/ui/bean/EmployeViewController.java
@@ -1,29 +1,45 @@
 package fr.imie.myrh.web.ui.bean;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 import fr.imie.myrh.dao.ifc.IEmployeDAO;
 import fr.imie.myrh.dao.impl.EmployeDAOImpl;
 import fr.imie.myrh.dao.model.Employe;
 
 @ManagedBean(name = "employeViewController")
+@ApplicationScoped()
 public class EmployeViewController implements Serializable {
 
 	private static final long serialVersionUID = -4592321803232225516L;
-	
+
 	Logger log = Logger.getLogger("EmmployeViewController");
 
 	private List<Employe> employeItems;
 
 	private final IEmployeDAO _employeDaoService = new EmployeDAOImpl();
-	
-	private Employe employe;
+
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	@ManagedProperty(value = "#{employe}")
+	private EmployeForm employe;
+
+	public EmployeForm getEmploye() {
+		return employe;
+	}
+
+	public void setEmploye(EmployeForm employe) {
+		this.employe = employe;
+	}
 
 	@PostConstruct
 	public void init() {
@@ -34,13 +50,37 @@ public class EmployeViewController implements Serializable {
 	public List<Employe> getEmployeItems() {
 		return employeItems;
 	}
-	
-	
-	
-	public Employe addEmploye(Employe employe) {
-		_employeDaoService.createEmploye(employe);
-		
-		return employe;
+
+	public String addEmploye() {
+		log.log(Level.INFO, "hello");
+		Employe model = new Employe();
+		model.setName(employe.getName());
+		model.setSurname(employe.getSurname());
+		/*try {
+			model.setBirthdate(sdf.parse(employe.getBirthdate()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			model.setArrival_date(sdf.parse(employe.getArrival_date()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			model.setDeparture_date(sdf.parse(employe.getDeparture_date()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		model.setSalary(employe.getSalary());
+		model.setNss(employe.getNss());
+		//model.setDepartement(employe.getDepartement());
+
+		_employeDaoService.createEmploye(model);
+
+		return "success";
 	}
 
 	public void setEmployeItems(List<Employe> employeItems) {
